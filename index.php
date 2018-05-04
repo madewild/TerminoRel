2d1e80a36abf253eb47cc9ef9e42ac3eb25ed02f
madewild <madewild@ulb.ac.be>
2018-05-04 15:24:19 +0200

    display table

--- index.php
+++ index.php
@@ -18,13 +18,19 @@ if ($conn) {
     mssql_select_db("terminorel", $conn);
     $query = mssql_query("SELECT * FROM biblio", $conn);
     if ($query) {
+        echo '<table><tr><th>ID</th><th>Titre</th><th>Type</th><th>Date</th><th>Source</th><th>Service</th></tr>';
         while ($row = mssql_fetch_assoc($query))
         {
             $ref = $row['reference'];
             //$title = mb_convert_encoding($row['title'], 'ISO-8859-1', 'windows-1252');
             $title = $row['title'];
-            echo $ref." : ". $title ."<br />";
+            $type = $row['typedoc'];
+            $date = $row['datedoc'];
+            $source = $row['source'];
+            $service = $row['service'];
+            echo '<tr><td>'.$ref.'</td><td>'.$title.'</td><td>'.$type.'</td><td>'.$date.'</td><td>'.$source.'</td><td>'.$service.'</td></tr>';
         }
+        echo '</table>';
     }
 }
 ?>
