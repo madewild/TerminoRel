0cff3214770d0d3da9a537860a36d9aaa59f0314
madewild <madewild@ulb.ac.be>
2018-06-04 22:32:38 +0200

    term sample term

---
+++ insert_term.php
@@ -0,0 +1,26 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
+ini_set('mssql.charset', 'UTF-8');
+include("secret.php");
+$server = SERVER;
+$username = USERNAME;
+$password = PASSWORD;
+$conn = mssql_connect($server, $username, $password);
+if ($conn) {
+    mssql_select_db("terminorel", $conn);
+    $xml = simplexml_load_file("xml/sample.xml");
+    foreach($xml->DC-209-terminologicalEntry as $doc)
+    {
+        $ref = $doc['DC-206-entryIdentifier'];
+        $subject = $doc->DC-489-subjectField;
+        $subject = str_replace("'", "''", $subject);
+        $cdu = $subject['cdu'];
+        $level = $subject['niveau'];
+        echo 'Importing ' . $ref . '...';
+        echo $subject . ' ' . $cdu . ' ' . $level;
+        //$query = mssql_query("INSERT INTO biblio (reference, title, typedoc, datedoc, source, service, url, filename) VALUES (N'$ref', N'$title', N'$type', '$date', N'$source', N'$service', N'$url', N'$filename')", $conn);
+        echo 'Done!';
+    }
+}
+?>
