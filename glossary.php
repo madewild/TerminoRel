2652bf93c4121eb7877516b60c308bda65b92a05
madewild <madewild@ulb.ac.be>
2018-06-06 11:37:31 +0200

    add star css

--- glossary.php
+++ glossary.php
@@ -4,10 +4,12 @@
 <form>
     <fieldset>
         <legend>Vos critères de recherche</legend>
-        Terme recherché
-        <input type="text" name="term" size="100">
-        <input type="submit" value="Rechercher">
+        Terme recherché<span class="star">*</span>
+        <input type="text" name="term" size="80">
+        <input type="submit" value="Rechercher"><br><br>
+        <span class="star">*</span>
+        <i>Ce symbole indique que le champ est obligatoire.</i>
     </fieldset>
-</form
+</form>
 
 <h3>Télécharger</h3>
