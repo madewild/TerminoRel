da9aef1e8e4891cda11dfde58a327019341d0b06
madewild <madewild@ulb.ac.be>
2018-06-06 11:26:14 +0200

    test form

--- glossary.php
+++ glossary.php
@@ -1,7 +1,14 @@
 <h2>Glossaires de l'ULB</h2>
 
 <h3>Rechercher</h3>
+<fieldset>
+    <legend>Vos critères de recherche</legend>
+    <form>
+        Terme recherché
+        <input type="text" name="term">
+        <input type="submit" value="Rechercher">
+    </form
+</fieldset>
 <?php
-echo "Vos critères de recherche<br><br>";
 ?>
 <h3>Télécharger</h3>
