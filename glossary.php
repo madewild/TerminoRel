ef2621cd30e6596c7040182389dd7c13b253945e
madewild <madewild@ulb.ac.be>
2018-06-06 11:29:44 +0200

    fieldset in form

--- glossary.php
+++ glossary.php
@@ -1,14 +1,13 @@
 <h2>Glossaires de l'ULB</h2>
 
 <h3>Rechercher</h3>
-<fieldset>
-    <legend>Vos critères de recherche</legend>
-    <form>
+<form>
+    <fieldset>
+        <legend>Vos critères de recherche</legend>
         Terme recherché
-        <input type="text" name="term">
+        <input type="text" name="term" size="100">
         <input type="submit" value="Rechercher">
-    </form
-</fieldset>
-<?php
-?>
+    </fieldset>
+</form
+
 <h3>Télécharger</h3>
