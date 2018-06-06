3af30cca3c7b789e2052c3a6b0c11415d5bf345f
madewild <madewild@ulb.ac.be>
2018-06-06 21:51:05 +0200

    add paragraphs

--- glossary.php
+++ glossary.php
@@ -4,17 +4,15 @@
 <form>
     <fieldset>
         <legend>Vos critères de recherche</legend>
-        <div class="groupLabel">
-            <label for="term">Terme recherché<span class="star">*</span>
-            <input type="text" class="input" id="term" size="80">
-            <input type="submit" value="Rechercher">
-        </div>
-        Langue source<span class="star">*</span>
-        <select>
+        <p><label for="term">Terme recherché<span class="star">*</span>
+        <input type="text" class="input" id="term" size="80">
+        <input type="submit" value="Rechercher"></p>
+        <p><label for="source">Langue source<span class="star">*</span>
+        <select id="source">
             <option value="fr">fr - français</option>
             <option value="en">en - anglais</option>
-        </select><br>
-        <span class="star">*</span> <i>Ce symbole indique que le champ est obligatoire.</i>
+        </select></p>
+        <p><span class="star">*</span> <i>Ce symbole indique que le champ est obligatoire.</i></p>
     </fieldset>
 </form>
 <br>
