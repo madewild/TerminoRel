a1788fb0f0985cf99917f386e98cf7e5dd8969be
madewild <madewild@ulb.ac.be>
2018-06-06 11:43:04 +0200

    add select

--- glossary.php
+++ glossary.php
@@ -6,10 +6,14 @@
         <legend>Vos critères de recherche</legend>
         Terme recherché<span class="star">*</span>
         <input type="text" name="term" size="80">
-        <input type="submit" value="Rechercher"><br><br>
-        <span class="star">*</span>
-        <i>Ce symbole indique que le champ est obligatoire.</i>
+        <input type="submit" value="Rechercher"><br>
+        Langue source<span class="star">*</span>
+        <select>
+            <option value="fr">fr - français</option>
+            <option value="en">en - anglais</option>
+        </select><br>
+        <span class="star">*</span> <i>Ce symbole indique que le champ est obligatoire.</i>
     </fieldset>
 </form>
-
+<br>
 <h3>Télécharger</h3>
