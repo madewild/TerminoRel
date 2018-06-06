3e1b40a6bb48962061a7680127fca08fc99cda56
madewild <madewild@ulb.ac.be>
2018-06-06 22:00:48 +0200

    full form

--- glossary.php
+++ glossary.php
@@ -4,14 +4,29 @@
 <form>
     <fieldset>
         <legend>Vos critères de recherche</legend>
-        <p><label for="term">Terme recherché<span class="star">*</span>
+        <p><label for="term">Terme recherché</label><span class="star">*</span>
         <input type="text" class="input" id="term" size="80">
         <input type="submit" value="Rechercher"></p>
-        <p><label for="source">Langue source<span class="star">*</span>
+        <p><label for="source">Langue source</label><span class="star">*</span>
         <select id="source">
             <option value="fr">fr - français</option>
             <option value="en">en - anglais</option>
         </select></p>
+        <p><label for="cible">Langues cibles</label><span class="star">*</span>
+        <input type="checkbox" id="cible" value="en" checked="checked"> en
+        <input type="checkbox" id="cible" value="fr"> fr
+        <input type="checkbox" id="cible" value="other"> ...
+        <input type="checkbox" id="cible" value="all"> Toutes</p>
+        <p><label for="domaine">Domaine</label>
+        <select id="domaine">
+            <option value="tf">Titres et fonctions</option>
+        </select></p>
+        <p><label for="type">Type d'information</label><br>
+        <input type="checkbox" id="type" value="trad" checked="checked"> Traduction
+        <input type="checkbox" id="type" value="syn" checked="checked"> Synonymes et acronymes
+        <input type="checkbox" id="type" value="def"> Définition (en français)
+        <input type="checkbox" id="type" value="ex" checked="checked"> Exemple d'usage
+        <input type="checkbox" id="type" value="all"> Toutes les informations</p>
         <p><span class="star">*</span> <i>Ce symbole indique que le champ est obligatoire.</i></p>
     </fieldset>
 </form>
