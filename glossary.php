35cf026993ccfe5ded2c7f467c2d161588d9b137
madewild <madewild@ulb.ac.be>
2018-06-06 11:18:46 +0200

    test mix html/php

--- glossary.php
+++ glossary.php
@@ -1,3 +1,7 @@
+<h2>Glossaires de l'ULB</h2>
+
+<h3>Rechercher</h3>
 <?php
-echo "<h1>Glossaires de l'ULB</h1><h2>Rechercher</h2><h3>Télécharger</h3>";
+echo "Vos critères de recherche<br><br>";
 ?>
+<h3>Télécharger</h3>
