1d172c2d459d51850ed959918f95dfcd0d247dc5
madewild <madewild@ulb.ac.be>
2018-06-07 23:32:42 +0200

    add langroup

--- insert_term.php
+++ insert_term.php
@@ -242,7 +242,7 @@ if ($conn) {
                 }
                 else {
                     echo("$langroup_id, N'$termlexid', N'$termtext', $pos_id, $gender_id, $status_id");
-                    $query = mssql_query("INSERT INTO termgroup (langroup, termlexid, termtext, pos, gender, qualifier) VALUES ('', N'$termlexid', N'$termtext', '', '', '')", $conn);
+                    $query = mssql_query("INSERT INTO termgroup (langroup, termlexid, termtext, pos, gender, qualifier) VALUES ($langroup_id, N'$termlexid', N'$termtext', '', '', '')", $conn);
                     $termgroup_id = mssql_insert_id();
                 }
 
