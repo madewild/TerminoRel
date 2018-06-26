26c96d20c849fc9f46ac21978f10724a0c0631ba
madewild <madewild@ulb.ac.be>
2018-06-26 22:29:35 +0200

    handle modified search

--- results.php
+++ results.php
@@ -1,15 +1,27 @@
-<?php $term = htmlspecialchars($_POST['term']); ?>
+<?php 
+$term = htmlspecialchars($_POST['term']);
+$source = htmlspecialchars($_POST['source']);
+$cible = htmlspecialchars($_POST['cible']);
+$domaine = htmlspecialchars($_POST['domaine']);
+$types = $_POST['type'];
+?>
+
 <h2>Glossaires de l'ULB</h2>
 
-<p><a href="#">Revenir à l'écran de recherche initial</a></p><br>
+<p><a href="http://terminorel.ulb.be">Revenir à l'écran de recherche initial</a></p><br>
 
-<p><form class="form" action="index.php" method="post">
+<p>
+    <form class="form" action="#" method="post">
         <input type="text" class="input" name="term" size="60" value="<?php echo $term ?>">
+        <input type="hidden" name="source" value="<?php echo $source; ?>">
+        <input type="hidden" name="cible" value="<?php echo $cible; ?>">
+        <input type="hidden" name="domaine" value="<?php echo $domaine; ?>">
+        <input type="hidden" name="type[]" value="<?php echo $types; ?>">
         <input type="submit" value="Rechercher"><br>
-        Langues : <?php echo htmlspecialchars($_POST['source']); ?> > <?php echo htmlspecialchars($_POST['cible']); ?> |
-        Domaine : <?php echo htmlspecialchars($_POST['domaine']); ?> |
+        <div class="small">
+        Langues : <?php echo $source ?> > <?php echo $cible ?> |
+        Domaine : <?php echo $domaine ?> |
         Type d'information : <?php 
-            $types = $_POST['type'];
             if(empty($types)) {
                 echo "Aucun";
             } else {
@@ -17,6 +29,8 @@
                 echo $typestring;
             }
         ?>
-</form></p>
+        </div>
+    </form>
+</p>
 
 <p>X entrées trouvées pour <?php echo $term; ?></p>
