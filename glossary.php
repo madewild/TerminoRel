b77e117b8270a658cc2adec33525dcdb8570e599
madewild <madewild@ulb.ac.be>
2018-06-26 22:20:31 +0200

    handle multiple types

--- glossary.php
+++ glossary.php
@@ -1,7 +1,7 @@
 <h2>Glossaires de l'ULB</h2>
 
 <h3>Rechercher</h3>
-<form class="form" action="index.php" method="post">
+<form class="form" action="#" method="post">
     <fieldset>
         <legend>Vos critères de recherche</legend>
         <p><label for="term">Terme recherché</label><span class="star">*</span>
@@ -22,11 +22,11 @@
             <option value="Titres et fonctions">Titres et fonctions</option>
         </select></p>
         <p><label for="type">Type d'information</label><br>
-        <input type="checkbox" name="type" value="Traduction" checked="checked"> Traduction
-        <input type="checkbox" name="type" value="Synonyme" checked="checked"> Synonymes et acronymes
-        <input type="checkbox" name="type" value="Définition"> Définition (en français)
-        <input type="checkbox" name="type" value="Exemple" checked="checked"> Exemple d'usage
-        <input type="checkbox" name="type" value="Toutes"> Toutes les informations</p><br>
+        <input type="checkbox" name="type[]" value="Traduction" checked="checked"> Traduction
+        <input type="checkbox" name="type[]" value="Synonyme" checked="checked"> Synonymes et acronymes
+        <input type="checkbox" name="type[]" value="Définition"> Définition (en français)
+        <input type="checkbox" name="type[]" value="Exemple" checked="checked"> Exemple d'usage
+        <input type="checkbox" name="type[]" value="Toutes"> Toutes les informations</p><br>
         <p><span class="star">*</span> <i>Ce symbole indique que le champ est obligatoire.</i></p>
     </fieldset>
 </form><br>
--- results.php
+++ results.php
@@ -1,13 +1,22 @@
+<?php $term = htmlspecialchars($_POST['term']); ?>
 <h2>Glossaires de l'ULB</h2>
 
-<p><a href="index.php">Revenir à l'écran de recherche initial</a></p><br>
+<p><a href="#">Revenir à l'écran de recherche initial</a></p><br>
 
 <p><form class="form" action="index.php" method="post">
-        <input type="text" class="input" name="term" size="60" value="<?php echo htmlspecialchars($_POST['term']); ?>">
-        <input type="submit" value="Rechercher">
-</form><br>
+        <input type="text" class="input" name="term" size="60" value="<?php echo $term ?>">
+        <input type="submit" value="Rechercher"><br>
+        Langues : <?php echo htmlspecialchars($_POST['source']); ?> > <?php echo htmlspecialchars($_POST['cible']); ?> |
+        Domaine : <?php echo htmlspecialchars($_POST['domaine']); ?> |
+        Type d'information : <?php 
+            $types = $_POST['type'];
+            if(empty($types)) {
+                echo "Aucun";
+            } else {
+                $typestring = implode(", ", $types);
+                echo $typestring;
+            }
+        ?>
+</form></p>
 
-Langues : <?php echo htmlspecialchars($_POST['source']); ?> > <?php echo htmlspecialchars($_POST['cible']); ?> |
-Domaine : <?php echo htmlspecialchars($_POST['domaine']); ?> |
-Type d'information : <?php echo htmlspecialchars($_POST['type']); ?>
-</p>
+<p>X entrées trouvées pour <?php echo $term; ?></p>
