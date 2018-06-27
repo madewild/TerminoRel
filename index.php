0708bf9b4d49ec64cdf616f775039a3166a446c0
madewild <madewild@ulb.ac.be>
2018-06-27 21:30:54 +0200

    add favicon

--- index.php
+++ index.php
@@ -103,21 +103,6 @@
   })();
 
       </script>
-
-<script>
-!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-document,'script','https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '117320265294612', {
-em: 'insert_email_variable,'
-});
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" src="https://www.facebook.com/tr?id=117320265294612&amp;ev=PageView&amp;noscript=1" style="display:none" width="1"></noscript>
-
-
    </head>
    <body onload="frameworkLoad()">
       <div id="bodyContainer">
