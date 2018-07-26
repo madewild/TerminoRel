5c42a7af40b70333b4a9e0d53a426a244be9333c
madewild <madewild@ulb.ac.be>
2018-07-26 22:48:43 +0200

    test contents of types

--- results.php
+++ results.php
@@ -82,13 +82,13 @@ if ($conn) {
                 echo "<tr><td colspan='2'>" . $definition . "</td></tr>";
             }
 
+            print_r($types);
+
             if(in_array("Contexte", $types)) {
                 $result = mssql_query("SELECT explanation FROM langroup WHERE termid=$termid AND lang=0", $conn);
                 $explanation = mssql_fetch_assoc($result)['explanation'];
                 echo "<tr><td colspan='2'>" . $explanation . "</td></tr>";
             }
-
-            // $explanation = mssql_fetch_assoc($result)['explanation'];
 
             echo "<tr><td><span class='target_lang'>EN</span></td>";
             echo "<td><b>" . $translation . "</b></td></tr>";
