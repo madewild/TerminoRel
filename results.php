b51015c95ab59444dbfd9a61cddec327e07e7668
madewild <madewild@ulb.ac.be>
2018-11-28 11:58:54 +0100

    add variant when acro

--- results.php
+++ results.php
@@ -99,7 +99,8 @@ if ($conn) {
             if($abbrev == 1) {
                 $result = mssql_query("SELECT * FROM termgroup WHERE langroup=$langroup_source", $conn);
                 $termtextfull = mssql_fetch_assoc($result)['termtext'];
-                echo " (<span class='term_text'>" . $termtextfull . "</span>)";
+                $termtextfull_variant = mssql_fetch_assoc($result)['variant'];
+                echo " (<span class='term_text'>" . $termtextfull . "|" . $termtextfull_variant . "</span>)";
             }
             echo "</td></tr>";
             
