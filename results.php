649567d760aa3d5b1103d1b01399b5735518f867
madewild <madewild@ulb.ac.be>
2018-11-28 12:49:10 +0100

    missing spaces

--- results.php
+++ results.php
@@ -131,7 +131,7 @@ if ($conn) {
                     $source_text = $row['text'];
                     $result = mssql_query("SELECT title FROM biblio WHERE id=$bib_id", $conn);
                     $bib_title = mssql_fetch_assoc($result)['title'];
-                    echo "<tr><td></td><td><u>Définition</u> : " . $definition . "(<i>" . $bib_title . "</i>, " . $source_text . ")</td></tr>";
+                    echo "<tr><td></td><td><u>Définition</u> : " . $definition . " (<i>" . $bib_title . "</i>, " . $source_text . ")</td></tr>";
                 }
             }
 
@@ -145,7 +145,7 @@ if ($conn) {
                     $source_text = $row['text'];
                     $result = mssql_query("SELECT title FROM biblio WHERE id=$bib_id", $conn);
                     $bib_title = mssql_fetch_assoc($result)['title'];
-                    echo "<tr><td></td><td><u>Explication</u> : " . $explanation . "(<i>" . $bib_title . "</i>, " . $source_text . ")</td></tr>";
+                    echo "<tr><td></td><td><u>Explication</u> : " . $explanation . " (<i>" . $bib_title . "</i>, " . $source_text . ")</td></tr>";
                 }
             }
 
