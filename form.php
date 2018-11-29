81dccb20e6d52b00d102755ca56611cb7adce241
madewild <madewild@ulb.ac.be>
2018-11-29 11:47:07 +0100

    remove stars

--- form.php
+++ form.php
@@ -4,15 +4,15 @@
 <form class="form" action="" method="post" onsubmit="return validate()">
     <fieldset>
         <legend>Vos critères de recherche</legend>
-        <p><label for="term">Terme recherché</label><span class="star">*</span>
+        <p><label for="term">Terme recherché</label><!--<span class="star">*</span>-->
         <input type="text" class="input" id="term" name="term" size="80">
         <input type="submit" value="Rechercher"></p>
-        <p><label for="source">Langue source</label><span class="star">*</span>
+        <p><label for="source">Langue source</label><!--<span class="star">*</span>-->
         <select id="source" name="source">
             <option value="fr">français</option>
             <option value="en">anglais</option>
         </select>
-        <label for="cible">Langue cible</label><span class="star">*</span>
+        <label for="cible">Langue cible</label><!--<span class="star">*</span>-->
         <select id="cible" name="cible">
             <option value="en">anglais</option>
         </select></p>
@@ -25,8 +25,8 @@
         <!--<p><label for="type">Informations additionnelles à afficher</label><br>
         <input type="checkbox" name="type[]" value="Définition"> Définition (en français)
         <input type="checkbox" name="type[]" value="Explication"> Explications supplémentaires (en français)
-        <input type="checkbox" name="type[]" value="Exemple"> Exemple(s) d'usage (en langue cible)-->
-        <p><span class="star">*</span> <i>Ce symbole indique que le champ est obligatoire.</i></p>
+        <input type="checkbox" name="type[]" value="Exemple"> Exemple(s) d'usage (en langue cible)
+        <p><span class="star">*</span> <i>Ce symbole indique que le champ est obligatoire.</i></p>-->
     </fieldset>
 </form><br>
 
