8ffd1d2ecab1158148bff4eee97f471511260911
Paul Muraille <paul.muraille@tictips.net>
2018-12-03 12:31:34 +0100

    about.php text upd

--- static/about.php
+++ static/about.php
@@ -1,12 +1,12 @@
-<h2>À propos des glossaires Terminorel</h2>
+<h2>À propos des glossaires TerminoRel</h2>
 
-<?php include('static/retour.php'); ?>
+<?php include('retour.php'); ?>
 
-<p>Les outils Terminorel offrent une aide à la décision rédactionnelle en langue anglaise. Ils permettent au traducteur ou au rédacteur de textes pour l'ULB vers l'anglais de vérifier s'il existe des équivalents recommandés ou simplement suggérés en anglais aux termes utilisés dans les textes en français.</p>
-<p>Ces aides sont au nombre de trois&nbsp;:
+<p>Les glossaires Terminorel offrent une aide à la décision au traducteur ou au rédacteur chargé de produire des textes en anglais intégrant du vocabulaire en relation avec l'ULB. Ces glossaires permettent de vérifier s'il existe, pour des termes utilisés dans les textes en français, des équivalents recommandés ou simplement suggérés en anglais.</p>
+<p>Ces glossaires peuvent être exploités de trois manières&nbsp;:
 <ul>
-<li>Outil de recherche&nbsp;: cet outil vous permet de rechercher s'il existe un ou plusieurs équivalents recommandés ou suggérés à un terme français. Il permet en outre, selon les termes recherchés, de visualiser la définition d'un terme (en français), un exemple d'usage du terme en anglais ou encore la déclinaison au féminin du terme (pour les titres et fonctions) ainsi que l'acronyme du terme s'il en existe un (en français et/ou en anglais).</li>
-<li>Glossaire alphabétique&nbsp;: cet outil vous donne accès à l'ensemble des termes français possédant un équivalent suggéré ou recommandé en anglais sous la forme d'une liste alphabétique.</li>
-<li>Fichier TBX&nbsp;: cet outil est spécifiquement destiné aux traducteurs et traductrices qui utilisent des outils de traduction assistée par ordinateur qui exploitent les glossaires au format TBX.</li>
+<li>Un formulaire de recherche vous permet de rechercher s'il existe un ou plusieurs équivalents recommandés ou suggérés à un terme français. Selon les termes recherchés, vous avez aussi la possibilité de visualiser la définition des termes (en français), un exemple d'usage du terme en anglais ou encore la déclinaison au féminin du terme (pour les titres et fonctions en français) ainsi que l'acronyme du terme s'il en existe un (en français et/ou en anglais).</li>
+<li>Un glossaire alphabétique vous donne accès à l'ensemble des termes français possédant un équivalent suggéré ou recommandé en anglais.</li>
+<li>Un fichier TBX avec le contenu des glossaires peut être téléchargé par les traducteurs et traductrices qui utilisent des outils de traduction assistée par ordinateur afin de disposer instantanément et en contexte des termes dont l'emploi est préconisé ou suggéré.</li>
 </ul>
 	</p>
