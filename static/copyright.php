869eacfa49ffe5abf685a66a947a952f621e62c8
madewild <madewild@ulb.ac.be>
2018-12-04 08:20:47 +0100

    subtitle should be h3

--- static/copyright.php
+++ static/copyright.php
@@ -2,5 +2,5 @@
 
 <?php include('static/retour.php'); ?>
 
-<h2>Données terminologiques au format TBX</h2>
+<h3>Données terminologiques au format TBX</h3>
 <p>Le fichier TBX (Termbase Exchange Format) et les données terminologiques qu'il contient sont mises à disposition sous le couvert de la licence Creative Commons 4.0, BY-NC-SA (attribution de paternité, pas de commercialisation et partage à l'identique).</p>
