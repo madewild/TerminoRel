abdc112545624eb57a46fbcfa2486f55c8145908
madewild <madewild@ulb.ac.be>
2018-12-04 21:22:02 +0100

    no line after last result

--- results.php
+++ results.php
@@ -221,7 +221,9 @@ if ($conn) {
                 }
             }
 
-            echo "<tr><td colspan='2'><hr style='border: solid 0.5px silver; height: 0px'></td></tr>";
+            if(--$num_rows > 0) {
+                echo "<tr><td colspan='2'><hr style='border: solid 0.5px silver; height: 0px'></td></tr>";
+            }
         }
         echo "</table>";
     }
