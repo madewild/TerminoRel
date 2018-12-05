67f8d3a76b8029132f922af22934e63bc3aa2da4
madewild <madewild@ulb.ac.be>
2018-12-05 10:47:02 +0100

    try new tbx

--- export.php
+++ export.php
@@ -8,12 +8,19 @@ $username = USERNAME;
 $password = PASSWORD;
 
 $tbx = '<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE martif PUBLIC "ISO 12200:1999A//DTD MARTIF core (DXFcdV04)//EN" "TBXcdv04.dtd">
-<martif type="TBX" xml:lang="en">
+<!DOCTYPE martif SYSTEM "TBXBasiccoreStructV02.dtd">
+<martif type="TBX-Basic" xml:lang="en">
   <martifHeader>
     <fileDesc>
+      <titleStmt>
+        <title>Terminorel – Glossaires de l’Université Libre de Bruxelles (Belgique)</title>
+        <note>Ce document est un glossaire destiné à aider les traducteurs et rédacteurs de textes de l’Université Libre de
+        Bruxelles pour le traitement vers l’anglais de textes rédigés en langue française (français de Belgique).</note>
+      </titleStmt>
       <sourceDesc>
-        <p>TerminoRel</p>
+        <p>Le contenu du document a été élaboré avec l’aide du centre de recherche Tradital (http://tradital.ulb.be/) de la
+        Faculté de Lettres, Traduction et Communication (http://www.ulb.ac.be/facs/ltc/index.html) de l’Université Libre de
+        Bruxelles (Belgique). Il est placé sous le régime de la licence Creative Commons 4.0, BY-NC-SA.</p>
       </sourceDesc>
     </fileDesc>
   </martifHeader>
@@ -23,27 +30,39 @@ $tbx = '<?xml version="1.0" encoding="UTF-8"?>
 $conn = mssql_connect($server, $username, $password);
 if ($conn) {
     mssql_select_db("terminorel", $conn);
-    $query = mssql_query("SELECT * FROM termgroup WHERE termlexid LIKE '%fr' ORDER BY termtext", $conn);
+    $query = mssql_query("SELECT * FROM term", $conn);
     $num_rows = mssql_num_rows($query);
     if ($num_rows > 0) {
         while ($row = mssql_fetch_assoc($query)) {
-            $term = $row['termtext'];
-            $lang = explode("-", $row['termlexid'])[3];
-            $langroup_source = $row['langroup'];
-            $result = mssql_query("SELECT termid FROM langroup WHERE id=$langroup_source", $conn);
-            $termid = mssql_fetch_assoc($result)['termid'];
+            $termid = $row['id'];
+            $ref = $row['reference'];
+            $tbx .= '
+      <termEntry id ="' . $ref . '">';
+            $result = mssql_query("SELECT * FROM subjectfield where term=$ref", $conn);
+            while ($row2 = mssql_fetch_assoc($query)) {
+              $subjectid = $row2['subject'];
+              $result = mssql_query("SELECT * FROM subject where id=$subjectid", $conn);
+              $subject = $result['text'];
+              $tbx .= '
+        <descrip type="subjectField">' . $subject . '</descrip>';
+            }
+            $result = mssql_query("SELECT id FROM langroup WHERE termid=$termid AND lang=0", $conn);
+            $langroup_source = mssql_fetch_assoc($result)['id'];
+            $result = mssql_query("SELECT termtext FROM termgroup WHERE langroup=$langroup_source", $conn);
+            $term = mssql_fetch_assoc($result)['termtext'];
+
             $result = mssql_query("SELECT id FROM langroup WHERE termid=$termid AND lang=1", $conn);
             $langroup_target = mssql_fetch_assoc($result)['id'];
             $result = mssql_query("SELECT termtext FROM termgroup WHERE langroup=$langroup_target", $conn);
             $translation = mssql_fetch_assoc($result)['termtext'];
+            
             $tbx .= '
-      <termEntry>
-        <langSet xml:lang="fr">
+        <langSet xml:lang="fr-BE">
           <tig>
             <term>'.$term.'</term>
           </tig>
         </langSet>
-        <langSet xml:lang="en">
+        <langSet xml:lang="en-UK">
           <tig>
             <term>'.$translation.'</term>
           </tig>
