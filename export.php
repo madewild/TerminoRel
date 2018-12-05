98b31cd4b9b54bf6f5d00c72e974f4a1ce099b73
madewild <madewild@ulb.ac.be>
2018-12-05 11:06:24 +0100

    fetch text

--- export.php
+++ export.php
@@ -41,8 +41,8 @@ if ($conn) {
             $result = mssql_query("SELECT * FROM subjectfield where term LIKE '$ref'", $conn);
             while ($row2 = mssql_fetch_assoc($result)) {
               $subjectid = $row2['subject'];
-              $result2 = mssql_query("SELECT * FROM subject where id=$subjectid", $conn);
-              $subject = $result2['text'];
+              $result2 = mssql_query("SELECT text FROM subject where id=$subjectid", $conn);
+              $subject = mssql_fetch_assoc($result2)['text'];
               $tbx .= '
         <descrip type="subjectField">' . $subject . '</descrip>';
             }
