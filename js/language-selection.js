be451ef5682d156d7b1a101ccd958db0d588e168
madewild <madewild@ulb.ac.be>
2019-06-20 22:28:28 +0200

    target in js script

--- js/language-selection.js
+++ js/language-selection.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
     $("#source").change(function () {
         var val = $(this).val();
         if (val == "en") {
-            $("#cible").html("<option value='fr'>français</option>");
+            $("#target").html("<option value='fr'>français</option>");
         } else if (val == "fr") {
-            $("#cible").html("<option value='en'>anglais</option>");
+            $("#target").html("<option value='en'>anglais</option>");
         }
     });
 });
