6c39788f1acc008e537472e05073c27ccfe4b399
madewild <madewild@ulb.ac.be>
2019-06-20 08:04:36 +0200

    integrate changes to glossary

--- glossary.php
+++ glossary.php
@@ -54,68 +54,107 @@ if ($conn) {
         while ($row = sqlsrv_fetch_array($query)) {
             echo "<tr>";
             $lang = strtoupper(explode("-", $row['termlexid'])[3]);
-            echo "<td><span class='source_lang'>" . $lang . "</span></td>";
-            echo "<td>" . $row['termtext'] . "";
+            $mf = False;
+            echo "<td width='45'>" . $lang . "</td>";
+            echo "<td><details><summary>";
+            echo "<span title='Cliquez sur le terme pour voir sa définition.'>" . $row['termtext'];
             $variant = $row['variant'];
             if($variant != NULL) {
-                echo " | " . $variant;
+                echo " | ";
+                echo $variant;
+                $mf = True;
             }
 
             $langroup_source = $row['langroup'];
             $abbrev = $row['abbrev'];
             if($abbrev == 1) {
                 $result = sqlsrv_query($conn, "SELECT * FROM termgroup WHERE langroup=$langroup_source", array(), array("Scrollable" => 'static'));
-                $row = sqlsrv_fetch_array($result);
-                $termtextfull = $row['termtext'];
-                $termtextfull_variant = $row['variant'];
+                $row2 = sqlsrv_fetch_array($result);
+                $termtextfull = $row2['termtext'];
+                $termtextfull_variant = $row2['variant'];
                 echo " (" . $termtextfull . " | " . $termtextfull_variant . ")";
+                $mf = True;
             } else {
                 $result = sqlsrv_query($conn, "SELECT * FROM termgroup WHERE langroup=$langroup_source AND abbrev=1", array(), array("Scrollable" => 'static'));
-                $row = sqlsrv_fetch_array($result);
-                if($row) {
-                    $acro = $row['termtext'];
+                $row2 = sqlsrv_fetch_array($result);
+                if($row2) {
+                    $acro = $row2['termtext'];
                     echo " (" . $acro . ")";
                 }
             }
+            echo "</span></summary>";
 
-            echo "</td></tr><tr>";
+            $pos_id = $row['pos'];
+            if($pos_id == 1) {
+                $pos = "Nom";
+            } else {
+                $pos = "Adjectif";
+            }
+            $gender_id = $row['gender'];
+            if($gender_id == 4 or $mf) {
+                $gender = "masculin ou féminin";
+            } else if($gender_id == 2) {
+                $gender = "masculin";
+            } else if($gender_id == 8) {
+                $gender = "féminin";
+            } else {
+                $gender = "";
+            }
+
+            echo "<br>" . $pos . " " . $gender . "<br>";
+
             $result = sqlsrv_query($conn, "SELECT termid FROM langroup WHERE id=$langroup_source", array(), array("Scrollable" => 'static'));
             $termid = sqlsrv_fetch_array($result)['termid'];
-            if($sort == "fr") {
-                $cible_id = 1;
-            } else {
-                $cible_id = 0;
+
+            $result = sqlsrv_query($conn, "SELECT definition FROM langroup WHERE termid=$termid AND lang LIKE 'fr%'", array(), array("Scrollable" => 'static'));
+            $definition = sqlsrv_fetch_array($result)['definition'];
+            if(!empty($definition)) {
+                $result = sqlsrv_query($conn, "SELECT * FROM source WHERE termid=$termid AND type='def'", array(), array("Scrollable" => 'static'));
+                $row = sqlsrv_fetch_array($result);
+                $bib_id = $row['biblio'];
+                $source_text_def = $row['text'];
+                $result = sqlsrv_query($conn, "SELECT title FROM biblio WHERE id=$bib_id", array(), array("Scrollable" => 'static'));
+                $bib_title_def = sqlsrv_fetch_array($result)['title'];
+                echo "<br><u>Définition</u> : " . $definition . " (<i>" . $bib_title_def . "</i>, " . $source_text_def . ")";
             }
-            $result = sqlsrv_query($conn, "SELECT id FROM langroup WHERE termid=$termid AND lang=$cible_id", array(), array("Scrollable" => 'static'));
+
+            $result = sqlsrv_query($conn, "SELECT explanation FROM langroup WHERE termid=$termid AND lang LIKE '$source%'", array(), array("Scrollable" => 'static'));
+            $explanation = sqlsrv_fetch_array($result)['explanation'];
+            if(!empty($explanation)) {
+                $result = sqlsrv_query($conn, "SELECT * FROM source WHERE termid=$termid AND type='exp'", array(), array("Scrollable" => 'static'));
+                $row = sqlsrv_fetch_array($result);
+                $bib_id = $row['biblio'];
+                $source_text_exp = $row['text'];
+                $result = sqlsrv_query($conn, "SELECT title FROM biblio WHERE id=$bib_id", array(), array("Scrollable" => 'static'));
+                $bib_title_exp = sqlsrv_fetch_array($result)['title'];
+                echo "<br><br><u>Explication</u> : " . $explanation . " (<i>" . $bib_title_exp . "</i>, " . $source_text_exp . ")";
+            }
+
+            echo "</details></td></tr>";
+
+            $result = sqlsrv_query($conn, "SELECT id FROM langroup WHERE termid=$termid AND lang LIKE '$cible%'", array(), array("Scrollable" => 'static'));
             $langroup_target = sqlsrv_fetch_array($result)['id'];
-            $results_recom = sqlsrv_query($conn, "SELECT * FROM termgroup WHERE langroup=$langroup_target AND qualifier!=5", array(), array("Scrollable" => 'static'));
+            $results_recom = sqlsrv_query($conn, "SELECT * FROM termgroup WHERE langroup=$langroup_target AND qualifier<>5", array(), array("Scrollable" => 'static'));
+            if($results_recom === FALSE) {
+                print_r(sqlsrv_errors(), true);
+                print_r($langroup_target);
+            }
             $results_prop = sqlsrv_query($conn, "SELECT * FROM termgroup WHERE langroup=$langroup_target AND qualifier=5", array(), array("Scrollable" => 'static'));
             $num_recom = sqlsrv_num_rows($results_recom);
-            while ($row = sqlsrv_fetch_array($results_recom)) {
-                $translation = $row['termtext'];
-                $lang_trad = strtoupper(explode("-", $row['termlexid'])[3]);
-                $variant = $row['variant'];
-                echo "<tr><td><span class='target_lang'>" . $lang_trad . "</span></td>";
-                    echo "<td><b>" . $translation;
-                    if($variant != NULL) {
-                        echo " | " . $variant;
-                    }
-                    echo "</b> (terme recommandé)</td></tr>";
-            }
-            if($num_recom == 0) {
-                while ($row = sqlsrv_fetch_array($results_prop)) {
-                    $translation = $row['termtext'];
-                    $lang_trad = strtoupper(explode("-", $row['termlexid'])[3]);
-                    $variant = $row['variant'];
-                    echo "<tr><td><span class='target_lang'>" . $lang_trad . "</span></td>";
-                    echo "<td><b>" . $translation;
-                    if($variant != NULL) {
-                        echo " | " . $variant;
-                    }
-                    echo "</b> (terme suggéré)</td></tr>";
+            echo "<tr><td>" . strtoupper($cible) . "</td><td>";
+            if($num_recom == 0 and $restriction == "approved_only") {
+                echo "Aucune traduction approuvée.";
+            } else {
+                show_trad($conn, $langroup_target, $results_recom, $cible, "recommandé");
+                if($num_recom == 0) {
+                    show_trad($conn, $langroup_target, $results_prop, $cible, "suggéré");
                 }
             }
-            echo "<tr><td colspan='2'></td></tr>";
+            echo "</td></tr>";
+
+            if(--$num_rows > 0) {
+                echo "<tr><th></th></tr><tr><th></th></tr>";
+            }
         }
         echo "</table>";
     }
