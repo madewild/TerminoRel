aafaccb95df838b85f6e2865e990c10cf42cd17c
madewild <madewild@ulb.ac.be>
2019-11-13 14:02:58 +0100

    force local jquery

--- index.php
+++ index.php
@@ -550,8 +550,7 @@
 
 </style>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="js/jquery.min.js"></script>')</script>
+    <script src="js/jquery.min.js"></script>
     <script src="js/validate.js"></script>
     <script src="js/details-shim.js"></script>
     <script src="js/language-selection.js?version=2"></script>
