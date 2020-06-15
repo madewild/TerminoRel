40f29fec7ccc2118a6d8ad4ef965801d5835db84
madewild <madewild@ulb.ac.be>
2020-06-15 23:04:26 +0200

    list terms

--- admin/terms.php
+++ admin/terms.php
@@ -6,7 +6,33 @@ $server = SERVER;
 $username = USERNAME;
 $password = PASSWORD;
 
+$conninfo = array(
+    "Database" => "terminorel",
+    "UID" => $username,
+    "PWD" => $password,
+    "CharacterSet" => "UTF-8"
+);
+
 #include("functions.php");
 include("../static/header.php");
 ?>
 <h3>Espace d'administration<h3>
+<form class="form" action="" method="get" onsubmit="return validate()">
+    <fieldset>
+        <legend>Vos critères de recherche</legend>
+        <nav id="form"><ul>
+            <li><label for="domain">Fiche</label>
+            <select name="fiche">
+                <?php
+                $conn = sqlsrv_connect($server, $conninfo);
+                if ($conn) {
+                    $query = sqlsrv_query($conn, "SELECT * FROM termgroup ORDER BY termtext", array(), array("Scrollable" => 'static'));
+                    while ($row = sqlsrv_fetch_array($query)) {
+                        echo '<option value="'.$row["termtext"].'">'.$row["termtext"].'</option>';
+                    }
+                }
+                ?>
+            </select></li>
+        </ul></nav>
+    </fieldset>
+</form><br>
