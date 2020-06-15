29d8ee46156188e19733582fcddd5cf3dd145888
madewild <madewild@ulb.ac.be>
2020-06-16 00:31:37 +0200

    24px

--- admin/details.php
+++ admin/details.php
@@ -18,8 +18,8 @@ $conninfo = array(
 include("../static/header.php");
 
 echo "<h3>Fiche détaillée 
-<img title='Modifier la fiche' src='icons/edit.png' style='width:32px;height:33px'> 
-<img title='Supprimer la fiche' src='icons/delete.png' style='width:32px;height:32px'></h3>";
+<img title='Modifier la fiche' src='icons/edit.png' style='width:24px;height:24px'> 
+<img title='Supprimer la fiche' src='icons/delete.png' style='width:24px;height:24px'></h3>";
 echo "<fieldset>";
 echo "<p>Terme principal en français : <b>" . $termtext . "</b></p>";
 echo "</fieldset>";
