c7840c1026e985568c10db6798371c92757579ca
madewild <madewild@ulb.ac.be>
2020-06-15 22:49:46 +0200

    add admin structure

---
+++ admin/terms.php
@@ -0,0 +1,12 @@
+<?php 
+error_reporting(-1);
+ini_set('display_errors', 'On');
+include("../static/secret.php");
+$server = SERVER;
+$username = USERNAME;
+$password = PASSWORD;
+
+#include("functions.php");
+include("../static/header.php");
+?>
+<h3>Espace d'administration<h3>
