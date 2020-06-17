edaf2849e1a0de1ad63a9f0d4a116773672950c2
madewild <madewild@ulb.ac.be>
2020-06-17 23:42:54 +0200

    use id and P01 default

--- admin/termlist.php
+++ admin/termlist.php
@@ -23,15 +23,23 @@ include("../static/header.php");
             <ul>
                 <li>
                     <label for="domain">Domaine</label>
-                    <select name="domain">
+                    <select id="domain" name="domain">
                         <option value="P01">Titres et fonctions</option>
                         <option value="P02">Enseignement supérieur</option>
                     </select>
                 </li>
                 <li>
                     <label for="fiche">Fiche</label>
-                    <select name="fiche">
-                        <option value="">Choisissez un domaine</option>
+                    <select id="fiche" name="fiche">
+                        <?php
+                        $conn = sqlsrv_connect($server, $conninfo);
+                        if ($conn) {
+                            $query = sqlsrv_query($conn, "SELECT * FROM termgroup WHERE termlexid LIKE 'P01%01-fr' ORDER BY termtext", array(), array("Scrollable" => 'static'));
+                            while ($row = sqlsrv_fetch_array($query)) {
+                                echo '<option value="'.$row["termlexid"].'">'.$row["termtext"].'</option>';
+                            }
+                        }
+                        ?>
                     </select>
                 </li>
                 <li><input type="submit" value="Afficher"></li>
