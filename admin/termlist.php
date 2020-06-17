71ebf5d84a57a2cfd87f9aa97d26da5fcbcc2391
madewild <madewild@ulb.ac.be>
2020-06-17 23:05:06 +0200

    add domain select with one value

--- admin/termlist.php
+++ admin/termlist.php
@@ -19,21 +19,30 @@ include("../static/header.php");
 <form class="form" action="" method="get">
     <fieldset>
         <legend>Vos critères de recherche</legend>
-        <nav id="form"><ul>
-            <li><label for="fiche">Fiche</label>
-            <select name="fiche">
-                <?php
-                $conn = sqlsrv_connect($server, $conninfo);
-                if ($conn) {
-                    $query = sqlsrv_query($conn, "SELECT * FROM termgroup WHERE termlexid LIKE 'P01%01-fr' ORDER BY termtext", array(), array("Scrollable" => 'static'));
-                    while ($row = sqlsrv_fetch_array($query)) {
-                        echo '<option value="'.$row["termlexid"].'">'.$row["termtext"].'</option>';
-                    }
-                }
-                ?>
-            </select>
-            <input type="submit" value="Afficher">
-            </li>
-        </ul></nav>
+        <nav id="form">
+            <ul>
+                <li>
+                    <label for="domain">Domaine</label>
+                    <select name="domain">
+                        <option value="P01">Titres et fonctions</option>
+                    </select>
+                </li>
+                <li>
+                    <label for="fiche">Fiche</label>
+                    <select name="fiche">
+                        <?php
+                        $conn = sqlsrv_connect($server, $conninfo);
+                        if ($conn) {
+                            $query = sqlsrv_query($conn, "SELECT * FROM termgroup WHERE termlexid LIKE 'P01%01-fr' ORDER BY termtext", array(), array("Scrollable" => 'static'));
+                            while ($row = sqlsrv_fetch_array($query)) {
+                                echo '<option value="'.$row["termlexid"].'">'.$row["termtext"].'</option>';
+                            }
+                        }
+                        ?>
+                    </select>
+                </li>
+                <li><input type="submit" value="Afficher"></li>
+            </ul>
+        </nav>
     </fieldset>
 </form><br>
