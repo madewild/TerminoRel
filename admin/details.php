9ed57e0d4fb1980d81e72539545bcf90f08081ce
madewild <madewild@ulb.ac.be>
2020-06-17 21:29:59 +0200

    add pos, gender, number

--- admin/details.php
+++ admin/details.php
@@ -32,6 +32,7 @@ $variant = $row['variant'];
 if($variant != NULL) {
     echo "<tr><td>Variante</td><td><b>" . $variant . "</b></td></tr>";
 }
+
 $langroup_source = $row['langroup'];
 $result = sqlsrv_query($conn, "SELECT * FROM termgroup WHERE langroup=$langroup_source AND abbrev=1", array(), array("Scrollable" => 'static'));
 $row2 = sqlsrv_fetch_array($result);
@@ -39,5 +40,36 @@ if($row2) {
     $acro = $row2['termtext'];
     echo "<tr><td>Abréviation</td><td><b>" . $acro . "</b></td></tr>";
 }
+
+$pos_id = $row['pos'];
+if($pos_id == 1) {
+    $pos = "nom";
+} else if($pos_id == 6) {
+    $pos = "adjectif"; // Also add Verbe
+} else {
+    $pos = "locution";
+}
+echo "<tr><td>Catégorie grammaticale</td><td><b>" . $pos . "</b></td></tr>";
+
+$gender_id = $row['gender'];
+if($gender_id == 4 or $mf) {
+    $gender = "masculin ou féminin";
+} else if($gender_id == 2) {
+    $gender = "masculin";
+} else if($gender_id == 8) {
+    $gender = "féminin";
+} else {
+    $gender = "";
+}
+echo "<tr><td>Genre</td><td><b>" . $gender . "</b></td></tr>";
+
+$number_id = $row['number'];
+if($number_id == 12) {
+    $number = "pluriel";
+} else {
+    $number = "singulier";
+}
+echo "<tr><td>Nombre</td><td><b>" . $number . "</b></td></tr>";
+
 echo "</table>";
 ?>
