451cae2c8fa8c3c416fb6d809fddb9b8e7b7b421
madewild <madewild@ulb.ac.be>
2020-06-25 23:32:08 +0200

    add delete page to main

--- admin/main.php
+++ admin/main.php
@@ -3,6 +3,8 @@ if(isset($_GET['fiche'])) {
     include("details.php");
 } else if(isset($_GET['update'])){
     include("update.php");
+} else if(isset($_GET['delete'])){
+    include("delete.php");
 } else if(isset($_GET['import'])){
     include("import.php");
 } else {
