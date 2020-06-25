41595cf1291cdc6f67e5647405fc31bcc43f1aa4
madewild <madewild@ulb.ac.be>
2020-06-25 21:19:17 +0200

    spacing

--- static/header.php
+++ static/header.php
@@ -1,13 +1,17 @@
 <?php
 echo "<h2>TerminoRel – Glossaires académiques de l'ULB pour la rédaction de textes en anglais</h2>";
+
 error_reporting(-1);
 ini_set('display_errors', 'On');
+
 include("functions.php");
 include("secret.php");
+
 $server = SERVER;
 $username = USERNAME;
 $password = PASSWORD;
 $path = $_SERVER['REQUEST_URI'];
+
 if (strpos($path, 'dev') !== false) {
     $database = "terminorel-dev";
 } else {
