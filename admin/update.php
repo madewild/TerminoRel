0b7b0576f6e6404f705e210a9fafad646e4fa578
madewild <madewild@ulb.ac.be>
2020-07-01 20:40:21 +0200

    150 px

--- admin/update.php
+++ admin/update.php
@@ -17,13 +17,13 @@ echo "<p><a href='" . $fullpath . "'>Retour à la fiche</a></p>";
 echo "<h3>Modifier la fiche</h3>";
 echo '<form class="form" action="" method="get"><fieldset><nav id="form"><table>';
 echo "<tr><td><label for='term'>Terme principal en français</label></td>";
-echo '<td><input type="text" class="input" id="term" name="term" value="' . $row['termtext'] . '" size="200"></td></tr>';
+echo '<td><input type="text" class="input" id="term" name="term" value="' . $row['termtext'] . '" size="150"></td></tr>';
 
 $variant = $row['variant'];
 $mf = False;
 if($variant != NULL) {
     echo "<tr><td><label for='variant'>Variante</label></td>";
-    echo '<td><input type="text" class="input" id="variant" name="variant" value="' . $variant . '" size="200"></td></tr>';
+    echo '<td><input type="text" class="input" id="variant" name="variant" value="' . $variant . '" size="150"></td></tr>';
     $mf = True;
 }
 
@@ -33,7 +33,7 @@ $row2 = sqlsrv_fetch_array($result);
 if($row2) {
     $acro = $row2['termtext'];
     echo "<tr><td><label for='abrev'>Abréviation</label></td>";
-    echo '<td><input type="text" class="input" id="abrev" name="abrev" value="' . $acro . '" size="200"></td></tr>';
+    echo '<td><input type="text" class="input" id="abrev" name="abrev" value="' . $acro . '" size="150"></td></tr>';
 }
 
 $pos_id = $row['pos'];
@@ -45,7 +45,7 @@ if($pos_id == 1) {
     $pos = "locution";
 }
 echo "<tr><td><label for='catgram'>Catégorie grammaticale</label></td>";
-echo '<td><input type="text" class="input" id="catgram" name="catgram" value="' . $pos . '" size="200"></td></tr>';
+echo '<td><input type="text" class="input" id="catgram" name="catgram" value="' . $pos . '" size="150"></td></tr>';
 
 $gender_id = $row['gender'];
 if($gender_id == 4 or $mf) {
@@ -58,7 +58,7 @@ if($gender_id == 4 or $mf) {
     $gender = "";
 }
 echo "<tr><td><label for='gender'>Genre</label></td>";
-echo '<td><input type="text" class="input" id="gender" name="gender" value="' . $gender . '" size="200"></td></tr>';
+echo '<td><input type="text" class="input" id="gender" name="gender" value="' . $gender . '" size="150"></td></tr>';
 
 $number_id = $row['number'];
 if($number_id == 12) {
@@ -67,7 +67,7 @@ if($number_id == 12) {
     $number = "singulier";
 }
 echo "<tr><td><label for='number'>Nombre</label></td>";
-echo '<td><input type="text" class="input" id="number" name="number" value="' . $number . '" size="200"></td></tr>';
+echo '<td><input type="text" class="input" id="number" name="number" value="' . $number . '" size="150"></td></tr>';
 
 $result = sqlsrv_query($conn, "SELECT termid FROM langroup WHERE id=$langroup_source", array(), array("Scrollable" => 'static'));
 $termid = sqlsrv_fetch_array($result)['termid'];
@@ -82,9 +82,9 @@ if(!empty($definition)) {
     $result = sqlsrv_query($conn, "SELECT title FROM biblio WHERE id=$bib_id", array(), array("Scrollable" => 'static'));
     $bib_title_def = sqlsrv_fetch_array($result)['title'];
     echo "<tr><td><label for='def'>Définition</label></td>";
-    echo '<td><input type="text" class="input" id="def" name="def" value="' . $definition . '" size="200"></td></tr>';
+    echo '<td><input type="text" class="input" id="def" name="def" value="' . $definition . '" size="150"></td></tr>';
     echo "<tr><td><label for='sourcedef'>Source de la définition</label></td>";
-    echo '<td><input type="text" class="input" id="sourcedef" name="sourcedef" value="' . $bib_title_def . ", " . $source_text_def . '" size="200"></td></tr>';
+    echo '<td><input type="text" class="input" id="sourcedef" name="sourcedef" value="' . $bib_title_def . ", " . $source_text_def . '" size="150"></td></tr>';
 }
 
 $result = sqlsrv_query($conn, "SELECT explanation FROM langroup WHERE termid=$termid AND lang LIKE 'fr%'", array(), array("Scrollable" => 'static'));
@@ -97,9 +97,9 @@ if(!empty($explanation)) {
     $result = sqlsrv_query($conn, "SELECT title FROM biblio WHERE id=$bib_id", array(), array("Scrollable" => 'static'));
     $bib_title_exp = sqlsrv_fetch_array($result)['title'];
     echo "<tr><td><label for='exp'>Explication</label></td>";
-    echo '<td><input type="text" class="input" id="exp" name="exp" value="' . $explanation . '" size="200"></td></tr>';
+    echo '<td><input type="text" class="input" id="exp" name="exp" value="' . $explanation . '" size="150"></td></tr>';
     echo "<tr><td><label for='sourceexp'>Source de l'explication</label></td>";
-    echo '<td><input type="text" class="input" id="sourceexp" name="sourceexp" value="' . $bib_title_exp . ", " . $source_text_exp . '" size="200"></td></tr>';
+    echo '<td><input type="text" class="input" id="sourceexp" name="sourceexp" value="' . $bib_title_exp . ", " . $source_text_exp . '" size="150"></td></tr>';
 }
 echo "</table>";
 
