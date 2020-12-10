f89a4f303a2cd2272cba01c3cf97570440036979
madewild <max.de.wilde@ulb.be>
2020-12-10 10:05:56 +0100

    add maintenance page

---
+++ static/maintenance.php
@@ -0,0 +1,13 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
+<style>
+.alert.alert-server {
+  margin-bottom: 0;
+  border-radius: 0;
+}
+</style>
+<div class="alert alert-danger alert-server" role="alert">
+  <button type="button" class="close" data-dismiss="alert">×</button>
+  <strong>Migration de serveur</strong> Le site sera temporairement indisponible du 17/12 à 17h au 18/12 à 17h, veuillez nous en excuser.
+</div>
