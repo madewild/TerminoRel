ddbbc04be4d237f9c0c2787bb46d11858b3b0357
madewild <max.de.wilde@ulb.be>
2023-10-02 23:25:22 +0200

    add paragraph around error

--- static/error.php
+++ static/error.php
@@ -2,4 +2,4 @@
 
 <?php include('static/retour.php'); ?>
 
-Désolé, il semble que la page à laquelle vous tentez d'accéder n'existe pas.
+<p>Désolé, il semble que la page à laquelle vous tentez d'accéder n'existe pas.</p>
