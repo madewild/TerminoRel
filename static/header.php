fce41ba27522d9070454673b2cfeb361842246ff
madewild <max.de.wilde@ulb.be>
2024-01-25 11:08:14 +0100

    display errors

--- static/header.php
+++ static/header.php
@@ -1,8 +1,9 @@
 <?php
 echo "<h2>TerminoRel – Glossaires académiques de l'ULB pour la rédaction de textes en anglais</h2>";
 
-error_reporting(-1);
-ini_set('display_errors', 'On');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 ini_set('pcre.jit', '0');
 
 include("functions.php");
