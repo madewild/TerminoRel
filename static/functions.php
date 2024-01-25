471c6d3e54b0632288d481d9313a8d6090b8b759
madewild <max.de.wilde@ulb.be>
2024-01-25 11:59:18 +0100

    move functions to mysqli

--- static/functions.php
+++ static/functions.php
@@ -7,10 +7,10 @@ function clean($string) {
     return $string;
 }
 
-function sqlsrv_insert_id($conn) {
+function mysqli_insert_id($conn) {
     $id = 0; 
-    $res = sqlsrv_query($conn, "SELECT @@identity AS id", array(), array("Scrollable" => 'static')); 
-    if ($row = sqlsrv_fetch_array($res)) { 
+    $res = mysqli_query($conn, "SELECT @@identity AS id", array(), array("Scrollable" => 'static')); 
+    if ($row = mysqli_fetch_array($res)) { 
         $id = $row["id"]; 
     }
     return $id; 
@@ -18,7 +18,7 @@ function sqlsrv_insert_id($conn) {
 
 function print_trad($results, $type) {
     $tig = '';
-    while ($row = sqlsrv_fetch_array($results)) {
+    while ($row = mysqli_fetch_array($results)) {
       $translation = $row['termtext'];
       $tig .= '
             <tig>
@@ -30,8 +30,8 @@ function print_trad($results, $type) {
   }
 
 function show_trad($conn, $results, $lang_trad, $type) {
-    $counter = sqlsrv_num_rows($results);
-    while ($row = sqlsrv_fetch_array($results)) {
+    $counter = mysqli_num_rows($results);
+    while ($row = mysqli_fetch_array($results)) {
         $color = "";
         if($type == "à éviter") { // quick fix, to improve later
             echo "<br>";
@@ -46,17 +46,17 @@ function show_trad($conn, $results, $lang_trad, $type) {
         echo "</b> (terme " . $type . ")";
         echo "</span></summary>";
         $termgroup = $row['id'];
-        $result = sqlsrv_query($conn, "SELECT id, context FROM contextgroup WHERE termgroup=$termgroup", array(), array("Scrollable" => 'static'));
-        $row = sqlsrv_fetch_array($result);
+        $result = mysqli_query($conn, "SELECT id, context FROM contextgroup WHERE termgroup=$termgroup", array(), array("Scrollable" => 'static'));
+        $row = mysqli_fetch_array($result);
         $contextgroup = $row['id'];
         $context = $row['context'];
         if(!empty($context)) {
-            $result = sqlsrv_query($conn, "SELECT * FROM source WHERE contextgroup=$contextgroup", array(), array("Scrollable" => 'static'));
-            $row = sqlsrv_fetch_array($result);
+            $result = mysqli_query($conn, "SELECT * FROM source WHERE contextgroup=$contextgroup", array(), array("Scrollable" => 'static'));
+            $row = mysqli_fetch_array($result);
             $bib_id = $row['biblio'];
             $source_text_con = $row['text'];
-            $result = sqlsrv_query($conn, "SELECT title FROM biblio WHERE id=$bib_id", array(), array("Scrollable" => 'static'));
-            $bib_title_con = sqlsrv_fetch_array($result)['title'];
+            $result = mysqli_query($conn, "SELECT title FROM biblio WHERE id=$bib_id", array(), array("Scrollable" => 'static'));
+            $bib_title_con = mysqli_fetch_array($result)['title'];
             echo "<br><u>Exemple d'usage</u> : « " . $context . " » (<i>" . $bib_title_con . "</i>, " . $source_text_con . ")";
         } else {
             echo "<br>Pas d'exemple d'usage pour ce terme.";
@@ -69,21 +69,21 @@ function show_trad($conn, $results, $lang_trad, $type) {
 }
 
 function show_trad_admin($conn, $results, $type) {
-    while ($row = sqlsrv_fetch_array($results)) {
+    while ($row = mysqli_fetch_array($results)) {
         $translation = $row['termtext'];
         echo "<table><tr><td><b>Traduction " . $type . "</b></td><td>" . $translation . "</td></tr>";
         $termgroup = $row['id'];
-        $result = sqlsrv_query($conn, "SELECT id, context FROM contextgroup WHERE termgroup=$termgroup", array(), array("Scrollable" => 'static'));
-        $row = sqlsrv_fetch_array($result);
+        $result = mysqli_query($conn, "SELECT id, context FROM contextgroup WHERE termgroup=$termgroup", array(), array("Scrollable" => 'static'));
+        $row = mysqli_fetch_array($result);
         $contextgroup = $row['id'];
         $context = $row['context'];
         if(!empty($context)) {
-            $result = sqlsrv_query($conn, "SELECT * FROM source WHERE contextgroup=$contextgroup", array(), array("Scrollable" => 'static'));
-            $row = sqlsrv_fetch_array($result);
+            $result = mysqli_query($conn, "SELECT * FROM source WHERE contextgroup=$contextgroup", array(), array("Scrollable" => 'static'));
+            $row = mysqli_fetch_array($result);
             $bib_id = $row['biblio'];
             $source_text_con = $row['text'];
-            $result = sqlsrv_query($conn, "SELECT title FROM biblio WHERE id=$bib_id", array(), array("Scrollable" => 'static'));
-            $bib_title_con = sqlsrv_fetch_array($result)['title'];
+            $result = mysqli_query($conn, "SELECT title FROM biblio WHERE id=$bib_id", array(), array("Scrollable" => 'static'));
+            $bib_title_con = mysqli_fetch_array($result)['title'];
             echo "<tr><td><b>Exemple d'usage</b></td><td>" . $context . "</td></tr>";
             echo "<tr><td><b>Source de l'example</b></td><td>" . $bib_title_con . ", " . $source_text_con . "</td></tr>";
         echo "</table>";
