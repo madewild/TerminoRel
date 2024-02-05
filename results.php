8f451c0c3b2a5b06de3b77db4dc461faaef0ed7a
madewild <max.de.wilde@ulb.be>
2024-02-05 11:36:34 +0100

    missing parenthesis

--- results.php
+++ results.php
@@ -39,7 +39,7 @@ mysqli_select_db($conn, $database) or die("Could not open the database '$databas
 
 if ($conn) {
     //$query = "SELECT * FROM termgroup WHERE termlexid LIKE '$refcode%$source' AND (termtext COLLATE FRENCH_CI_AI LIKE '%$clean_term%' COLLATE FRENCH_CI_AI OR variant COLLATE FRENCH_CI_AI LIKE '%$clean_term%' COLLATE FRENCH_CI_AI) ORDER BY termtext";
-    $query = "SELECT * FROM termgroup WHERE termlexid LIKE '$refcode%$source' AND (termtext LIKE '%$clean_term%' OR variant LIKE '%$clean_term%' ORDER BY termtext";
+    $query = "SELECT * FROM termgroup WHERE termlexid LIKE '$refcode%$source' AND (termtext LIKE '%$clean_term%' OR variant LIKE '%$clean_term%') ORDER BY termtext";
     echo($query);
     $result = mysqli_query($conn, $query);
     $num_rows = mysqli_num_rows($result);
