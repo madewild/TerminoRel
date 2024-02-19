478b919cc6bf4beeb17ca49d2763d6599da497f3
madewild <max.de.wilde@ulb.be>
2024-02-19 22:01:05 +0100

    debug ajax

--- admin/ajaxData.php
+++ admin/ajaxData.php
@@ -7,7 +7,9 @@ if(!empty($_POST["domain_id"])){
     $conn -> set_charset("utf8");
     if ($conn) {
         $query = "SELECT * FROM termgroup WHERE termlexid LIKE '$domainid%01-fr' ORDER BY termtext";
+        echo($query);
         $main_result = mysqli_query($conn, $query);
+        print_r($main_result);
         while ($row = mysqli_fetch_assoc($main_result)) {
             echo '<option value="'.$row["termlexid"].'">'.$row["termtext"].'</option>';
         }
