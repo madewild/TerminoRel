0de0cf960463c490650f91f92247ebd79d684c26
madewild <max.de.wilde@ulb.be>
2024-02-19 22:03:50 +0100

    select db first

--- admin/ajaxData.php
+++ admin/ajaxData.php
@@ -5,11 +5,10 @@ if(!empty($_POST["domain_id"])){
     $domainid = $_POST["domain_id"];
     $conn = mysqli_connect($server, $username, $password) or die("Unable to connect to '$server'");
     $conn -> set_charset("utf8");
+    mysqli_select_db($conn, $database) or die("Could not open the database '$database'");
     if ($conn) {
         $query = "SELECT * FROM termgroup WHERE termlexid LIKE '$domainid%01-fr' ORDER BY termtext";
-        echo($query);
         $main_result = mysqli_query($conn, $query);
-        print_r($main_result);
         while ($row = mysqli_fetch_assoc($main_result)) {
             echo '<option value="'.$row["termlexid"].'">'.$row["termtext"].'</option>';
         }
